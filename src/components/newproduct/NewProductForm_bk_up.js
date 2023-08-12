7c994c529bcaf75017295a03addc08d8fad030c2
Eliud Gateri <egateri@gmail.com>
2023-08-13 01:29:10 +0300

    Covert image to base64 and upload

--- src/components/newproduct/NewProductForm_bk_up.js
+++ src/components/newproduct/NewProductForm_bk_up.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './newProductForm.css';
-import FileBase from 'react-file-base64';
+
 import {v4} from 'uuid'; 
-import { useState } from 'react';
 
 const NewProductForm = (props) =>{
-const[listingData, setListingData] = useState("");
 
        // Function for handling onsubmission event
 const handleNewProductFormSubmission =(event)=>{
@@ -14,16 +12,13 @@ const handleNewProductFormSubmission =(event)=>{
     console.log(event.target.price.value)
     console.log(event.target.description.value)
     console.log(event.target.quantity.value)
-    // console.log(event.target.photo.value)
-    console.log(listingData?.selectedFile)
-    const photo =listingData?.selectedFile;
+
     props.onNewProductCreation({
         name: event.target.name.value,
         price: event.target.price.value,
         description: event.target.description.value,
         quantity: event.target.quantity.value,
         // photo: event.target.photo.value,
-        photo: photo,
         id: v4()
     })
     }
@@ -77,20 +72,16 @@ const handleNewProductFormSubmission =(event)=>{
                        <label htmlFor="quantity"> Quantity</label>
                    </div>
                    {/* <div className="form-input-material">
-                       <input type = 'file'
+                       <input type = 'text'
                            name = 'photo'
-                           placeholder = ''
+                           placeholder = ' '
                            className = 'form-control-material'
                            autoComplete="off"
                            id = 'photo'
-                           accept=".jpg, .jpeg, .png"
                            required
                        />
                        <label htmlFor="quantity"> Photo</label>
                    </div> */}
-                   <div className="form-input-material">
-                   <FileBase type="file" multiple={false} onDone={({base64}) => setListingData({ ...listingData, selectedFile: base64})}/>
-                   </div>
                    <button type="submit" className="btn btn-primary btn-ghost">Add Product</button>   
                </form>
            </div>
